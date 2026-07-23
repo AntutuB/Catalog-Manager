6d32361b29281f2bce04c43877ebfc502a4c16f6
Antutu <imantutub@gmail.com>
2026-07-23 14:20:32 -0400

    Define template runtime schema

--- src/templates/schema/template.schema.js
+++ src/templates/schema/template.schema.js
@@ -3,20 +3,51 @@ const TemplateSchema = {
     required: [
 
         "id",
-
         "name",
-
         "version",
-
         "canvas",
-
         "theme",
-
         "fonts",
-
         "assets",
-
         "elements"
+
+    ],
+
+    elementRequired: [
+
+        "id",
+        "type",
+        "frame"
+
+    ],
+
+    elementTypes: [
+
+        "background",
+
+        "asset",
+
+        "image",
+
+        "text",
+
+        "shape"
+
+    ],
+
+    bindings: [
+
+        "product.name",
+
+        "product.price",
+
+        "product.category",
+
+        "product.description",
+
+        "product.brand",
+
+        "product.image"
 
     ]
 
